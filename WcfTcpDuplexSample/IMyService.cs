e58cfa1092793e200ea686377b5a6f899f09d7de
satohnorio <satohnorio@gmail.com>
2015-10-26 21:02:07 +0900

    双方向通信サンプルを作成 アプリケーション構成からクライアントのサービス参照を可能にするサンプルを作成。

---
+++ WcfTcpDuplexSample/IMyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+
+namespace Gsf.Samples.WCF
+{
+    [ServiceContract(
+        Namespace="http://Gsf.Samples.WCF", 
+        CallbackContract=typeof(IMyCallback))]
+    public interface IMyService
+    {
+        [OperationContract(IsOneWay=true)]
+        void Regist(string name);
+    }
+}
---
+++ WcfTcpDuplexSample/MyService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+
+namespace Gsf.Samples.WCF
+{
+    public class MyService : IMyService
+    {
+        public void Regist(string name)
+        {
+            Console.Write("[CALL] Regist() => ");
+
+            var context  = OperationContext.Current;
+            var callback = context.GetCallbackChannel<IMyCallback>();
+
+            Console.WriteLine("[CALLBACK] SendData();");
+            callback.SendData(string.Format("{0}_Callback", name));
+        }
+    }
+}
